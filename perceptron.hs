831faeb3f873c437fbe575524fe9ea967af679f4
Takeo Miyamoto <miyamofigo@gmail.com>
2016-10-20 13:14:19 +0000

    resolved name collisions

--- perceptron.hs
+++ perceptron.hs
@@ -7,10 +7,10 @@ op ws b x1 x2 = case (sumElements (x * w) + b) > 0 of
  where x = vector [x1, x2]
        w = vector [fst ws, snd ws]
 
-and  x1 x2 = op (0.5, 0.5) (-0.7) x1 x2
-nand x1 x2 = op (-0.5, -0.5) 0.7 x1 x2
-or   x1 x2 = op (0.5, 0.5) (-0.2) x1 x2
+pAnd  x1 x2 = op (0.5, 0.5) (-0.7) x1 x2
+pNand x1 x2 = op (-0.5, -0.5) 0.7 x1 x2
+pOr   x1 x2 = op (0.5, 0.5) (-0.2) x1 x2
 
-xor  x1 x2 = Main.and (fromIntegral s1) (fromIntegral s2)
- where s1 = Main.nand x1 x2
-       s2 = Main.or x1 x2  
+pXor  x1 x2 = pAnd (fromIntegral s1) (fromIntegral s2)
+ where s1 = pNand x1 x2
+       s2 = pXor x1 x2  
